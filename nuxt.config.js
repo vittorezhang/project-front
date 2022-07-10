ce16b8de4ae57b8e0f836a58df724a9ed101316d
zwt <yc_zwt@163.com>
2022-07-10 11:59:35 +0800

    🌷 update: title 修改

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,8 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    // title: process.env.npm_package_name || '',
+		title: '小开社区',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
