05d4773c61f462696dd017d147a5d7e8635f8c93
zwt <yc_zwt@163.com>
2022-07-11 23:38:16 +0800

    💥 feat: 添加axios请求拦截

--- plugins/axios.js
+++ plugins/axios.js
@@ -10,6 +10,22 @@ let service = axios.create({
 // 主要做token的管理
 Vue.prototype.$http =service
 
+// 请求拦截
+service.interceptors.request.use(
+	config=>{
+		// 请求加token
+		const token = window.localStorage.getItem(TOKEN_KEY)
+		// 设置url白名单
+		if(token){
+			config.headers.common['Authorization'] = 'Bearer '+token
+		}
+		return config
+	},
+	err=>{
+		return Promise.reject(err)
+	}
+)
+
 // 响应拦截
 service.interceptors.response.use(
 	async response=>{
