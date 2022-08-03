d65eeba6d0f7d5c870a4660a2fafee6c08623404
zwt <yc_zwt@163.com>
2022-08-03 09:17:24 +0800

    💥 feat: 完善mutations

--- store/user.js
+++ store/user.js
@@ -12,13 +12,20 @@ const state = ()=>({
 
 const mutations = {
   SET_TOKEN(state, token){
-
+    state.token = token
   },
   SET_USER(state,user){
-    
+    state.id = user._id
+    state.email = user.email
+    state.nickname = user.nickname
+    state.avatar = user.avatar
   },
   LOGOUT(state){
-   
+    state.id = ''
+    state.email = ''
+    state.nickname = ''
+    state.avatar = ''
+    state.token  = ''
   }
 }
 
