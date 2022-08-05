e64722942914a5ab9f7407fffa27b0428f35065d
zwt <yc_zwt@163.com>
2022-08-05 09:51:29 +0800

    💥 feat: 添加用户中心

---
+++ pages/uc.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>用户中心</h1>
+    <div ref='drag' id="drag">
+      <input type="file" name="file" @change="handleFileChange">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      file:null
+    }
+  },
+  computed:{
+
+  },
+  methods:{
+    handleFileChange(e){
+      const [file] = e.target.files
+      if(!file) return 
+      this.file = file
+    }
+  }
+}
+</script>
