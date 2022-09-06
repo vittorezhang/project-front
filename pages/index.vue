fbc7f1832d28b6808c4c96137b4f34ed6c9269e1
zwt <yc_zwt@163.com>
2022-09-06 23:56:23 +0800

    UI: 样式修改

--- pages/index.vue
+++ pages/index.vue
@@ -55,14 +55,14 @@ export default {
 </script>
 
 <style>
-.container {
+/* .container {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
+} */
 
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
